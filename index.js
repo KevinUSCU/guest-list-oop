31632c7b54fcfbaee19c66336388f2197e18f579
Kevin Springer <KevinUSCU@outlook.com>
2017-11-22 13:14:47 -0800

    Improve Party.enter.

--- index.js
+++ index.js
@@ -39,14 +39,17 @@ class Party {
   }
 
   enter (guest) {
-    this.currentGuests.push(guest)
+    if (this.currentGuests.length >= this.venue.capacity) console.log(`We're full; no one else may enter!`)
+    else if (this.guestList.guests.includes(guest)) this.currentGuests.push(guest)
+    else console.log(`Whoa there! ${guest.name} is not allowed and has been denied entry!`)
   }
 
   leave (guest) {
     const index = this.currentGuests.indexOf(guest)
-    this.currentGuests.splice(index, 1)
-
-    this.pastGuests.push(guest)
+    if (index !== -1) {
+      this.currentGuests.splice(index, 1)
+      this.pastGuests.push(guest)
+    }
   }
 
   get population () {
@@ -60,14 +63,17 @@ const matt = new Guest('Matt', true)
 const sean = new Guest('Sean', true)
 const wes = new Guest('Wes', true)
 
-const venue = new Venue(25)
+// const venue = new Venue(25)
+const venue = new Venue(3)
 const guestList = new GuestList()
 
 guestList.addGuests(kevin, nathan, matt, sean)
 
 const party = new Party(venue, guestList)
 party.enter(matt)
+console.log(party)
 party.enter(sean)
 party.leave(sean)
+party.enter(wes)
 
 console.log(party)
